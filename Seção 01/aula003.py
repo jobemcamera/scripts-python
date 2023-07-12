919a110568696c36d9a95c6e12a8ed6748820c5a
Jobe Moraes <jobecamera@gmail.com>
2023-07-12 11:33:02 -0300

    tipos de dados em python

---
+++ Seção 01/aula003.py
@@ -0,0 +1,13 @@
+# Tipos int e float
+
+# int
+print(11)
+print(-11)
+print(5454)
+
+# float
+print(1.1)
+print(-89.6)
+
+# type
+print(type(1), type(6.6), type('Jobe'), type(True), type([]), type({}))
