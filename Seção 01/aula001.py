73f2a27a23f62a8ec9f34371420039904f658409
Jobe Moraes <jobecamera@gmail.com>
2023-07-12 11:21:57 -0300

    strings em python

---
+++ Seção 01/aula001.py
@@ -0,0 +1,15 @@
+# Strings
+
+# Aspas simples
+print('Jobe Camera')
+print('Jobe "Camera"')
+
+# Aspas duplas
+print("Jobe Camera")
+print("Jobe 'Camera'")
+
+# Escape
+print("Jobe \"Camera\"")
+
+# r
+print(r"Jobe \"Camera\"")
