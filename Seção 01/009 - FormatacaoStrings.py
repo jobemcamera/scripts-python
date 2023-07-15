f936679f12d681c443a7e0df2e57a42374f12ecb
Jobe Moraes <jobecamera@gmail.com>
2023-07-15 09:48:39 -0300

    formatacao de strings format

--- Seção 01/009 - FormatacaoStrings.py
+++ Seção 01/009 - FormatacaoStrings.py
@@ -7,3 +7,21 @@ imc = peso / altura ** 2
 
 mensagem = f'{nome} mede {altura:.2f}m e pesa {peso:.2f}kg. Seu IMC é de {imc:.2f}.'
 print(mensagem)
+
+
+# Formatação de Strings format
+
+a = 'A'
+b = 'B'
+c = '1.1'
+
+formato = '{} mede {:.2f}m e pesa {:.2f}kg. Seu IMC é de {:.2f}.'.format(nome, altura, peso, imc)
+print(formato)
+
+# Caso precise de mais chamadas do mesmo parâmetro, use os índices.
+formato = '{0} mede {1:.2f}m e pesa {2:.2f}kg. Seu IMC é de {3:.2f}.'.format(nome, altura, peso, imc)
+print(formato)
+
+# Parâmetro nomeados. Se nomear um parâmetros, todos precisam ser nomeados.
+formato = '{n} mede {h:.2f}m e pesa {p:.2f}kg. Seu IMC é de {imc:.2f}.'.format(n=nome, h=altura, p=peso, imc=imc)
+print(formato)
