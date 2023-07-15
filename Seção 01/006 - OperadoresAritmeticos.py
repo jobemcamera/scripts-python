1a79cd2cd888ddb01b08cdfc65dd6d193214c723
Jobe Moraes <jobecamera@gmail.com>
2023-07-15 09:26:37 -0300

    operadores aritmeticos

---
+++ Seção 01/006 - OperadoresAritmeticos.py
@@ -0,0 +1,22 @@
+# Operadores aritméticos
+
+adicao = 10 + 10
+print('Adição', adicao)
+
+subtracao = 15 - 10
+print('Subtracao', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicacao', multiplicacao)
+
+divisao = 10 / 2.2
+print('Divisão', divisao) # sempre é float
+
+divisao_inteira = 10 // 2.2
+print('Divisão Inteira', divisao_inteira) # pode ser int ou float (truncado)
+
+exponenciacao = 2 ** 10
+print('Exponenciação', exponenciacao)
+
+modulo = 25 % 5
+print('Módulo', modulo)
