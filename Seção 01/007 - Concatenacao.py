ed9e039f6195f82bd61f20129ac8df870973f038
Jobe Moraes <jobecamera@gmail.com>
2023-07-15 09:29:05 -0300

    concatenacao operadores

---
+++ Seção 01/007 - Concatenacao.py
@@ -0,0 +1,10 @@
+# Concatenacao - operadores
+
+concatenacao = 'Jobe' + ' ' + 'Camera'
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10
+print(a_dez_vezes)
+
+jobe_vezes = 'Jobe' * 5
+print(jobe_vezes)
