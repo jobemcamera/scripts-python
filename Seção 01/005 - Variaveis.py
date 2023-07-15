6ceeeaee0fca1bfe9e43f1eb407fd459c7cadecb
Jobe Moraes <jobecamera@gmail.com>
2023-07-15 09:18:00 -0300

    variaveis

---
+++ Seção 01/005 - Variaveis.py
@@ -0,0 +1,9 @@
+# Variáveis
+
+# nome_variavel
+
+nome_completo = 'Jobe Camera'
+soma_numeros = 65 + 89
+idade = 29
+maior_idade = idade >= 18
+print(nome_completo, soma_numeros, idade, maior_idade)
