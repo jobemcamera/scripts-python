f27543061f1fb2a99730c1fde7096177d4c449a8
Jobe Moraes <jobecamera@gmail.com>
2023-07-15 09:33:26 -0300

    precedencia operadores

---
+++ Seção 01/008 - Precedencia.py
@@ -0,0 +1,12 @@
+# Precedência operadores
+
+# 1 - (n + n)
+# 2 - **
+# 3 - * / // %
+# 4 - + -
+
+conta_1 = 1 + 1 ** 5 + 5 # Resultado = 7
+print(conta_1)
+
+conta_2 = 1 + (2*6 / 5) % 2 # Resultado = 1.4
+print(conta_2)
