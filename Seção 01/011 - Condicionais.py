8928498391ea0d7251fd2efe0708b8c7b03165ea
Jobe Moraes <jobecamera@gmail.com>
2023-07-16 09:02:58 -0300

    condicionais em python

---
+++ Seção 01/011 - Condicionais.py
@@ -0,0 +1,10 @@
+# Condicionais - if / else / elif
+
+entrada = input('Você quer "entrar" ou "sair"? ')
+
+if entrada == 'entrar':
+  print('Você entrou no sistema.')
+elif entrada == 'sair':
+  print('Você saiu do sistema.')
+else:
+  print('Você nem entrou nem saiu do sistema xD')
