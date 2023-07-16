2ee21bc721f373cb67077841585aa70a356cbfc3
Jobe Moraes <jobecamera@gmail.com>
2023-07-16 08:58:08 -0300

    inputs em python

---
+++ Seção 01/010 - Inputs.py
@@ -0,0 +1,15 @@
+# Inputs
+
+nome = input('Qual o seu nome? ')
+print(f'Olá, {nome}')
+
+idade = int(input('Qual sua idade? '))
+print(f'Você tem {idade} anos de idade')
+
+numero_1 = input('Digite um número: ')
+numero_2 = input('Digite outro número: ')
+
+int_numero_1 = int(numero_1)
+int_numero_2 = int(numero_2)
+
+print(f'A soma dos números {int_numero_1} e {int_numero_2} é: {int_numero_1 + int_numero_2}')
