ccb90d39af565a6503047389ade50a57b69515c8
Jobe Moraes <jobecamera@gmail.com>
2023-07-18 11:13:17 -0300

    novos tipos de formatacao de strings em python

--- Seção 01/009 - FormatacaoStrings.py
+++ Seção 01/009 - FormatacaoStrings.py
@@ -25,3 +25,20 @@ print(formato)
 # Parâmetro nomeados. Se nomear um parâmetros, todos precisam ser nomeados.
 formato = '{n} mede {h:.2f}m e pesa {p:.2f}kg. Seu IMC é de {imc:.2f}.'.format(n=nome, h=altura, p=peso, imc=imc)
 print(formato)
+
+# Outras formatações
+
+# padding
+variavel = 'ABC'
+print(f'{variavel}')
+print(f'{variavel: >10}.')
+print(f'{variavel: <10}.')
+print(f'{variavel: ^10}.')
+print(f'{1000.48565656:.1f}')
+print(f'{1000.48565656:,.1f}') # mostra a virgula para separar milhar
+print(f'{1000.48565656:-,.1f}') # mostra o sinal negativo
+print(f'{1000.48565656:+,.1f}') # mostra o sinal positivo
+print(f'{1000.48565656:0=+10,.1f}')
+print(f'O hexadecimal de 1500 é {1500:08X}') # maiúsculo
+print(f'O hexadecimal de 1500 é {1500:08x}') # minúsculo
+print(f'{variavel!r}') # conversion flags - !r !s !a
