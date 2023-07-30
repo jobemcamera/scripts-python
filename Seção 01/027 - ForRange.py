7105560a7dd49e1838b489daf2adb7f31b5d3fea
Jobe Moraes <jobecamera@gmail.com>
2023-07-30 19:04:08 -0300

    for e range em python

---
+++ Seção 01/027 - ForRange.py
@@ -0,0 +1,22 @@
+# For / Range
+
+# numeros de 0 a 10
+numeros = range(10)
+for numero in numeros:
+  print(numero) 
+  
+# numeros de 5 a 10
+numeros = range(5, 10)
+for numero in numeros:
+  print(numero) 
+  
+# numeros de 0 a 10 pulando de 2
+numeros = range(5, 10, 2)
+for numero in numeros:
+  print(numero) 
+
+# numeros de 10 a 2 pulando de -2
+numeros = range(10, 0, -2)
+for numero in numeros:
+  print(numero) 
+
